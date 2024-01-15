bcbf8501b859d61528d2e213f1fbf9e3aa322e8a
well-john <lu.well.john06@gmail.com>
2024-01-15 14:33:06 +0100

    lll

--- tp_clustering.py
+++ tp_clustering.py
@@ -4,6 +4,7 @@ from scipy.io import arff
 import time
 from sklearn import cluster
 
+
 # Parser un fichier de donnees au format arff
 # data est un tableau d ’ exemples avec pour chacun
 # la liste des valeurs des features
@@ -19,7 +20,7 @@ from sklearn import cluster
 # numero de cluster . On retire cette information
 
 path = './clustering-benchmark/src/main/resources/datasets/artificial/'
-databrut = arff.loadarff(open(path + "xclara.arff" , 'r'))
+databrut = arff.loadarff(open(path + "impossible.arff" , 'r')) #triangle1
 datanp = [[ x[0],x[1]] for x in databrut [0]]
 
 # Affichage en 2D
@@ -41,7 +42,7 @@ plt.show()
 
 print ("Appel KMeans pour une valeur fixee de k")
 tps1 = time.time()
-k = 3
+k = 4
 model = cluster.KMeans(n_clusters =k,init ='k-means++')
 model.fit(datanp)
 tps2 = time.time()
@@ -52,3 +53,46 @@ plt.scatter (f0,f1,c = labels , s=8)
 plt.title("Donnees apres clustering Kmeans")
 plt.show()
 print("nb clusters =" ,k , " , nb iter = " , iteration , " ,... ...runtime = " , round (( tps2 - tps1 ) * 1000 , 2 ) ," ms ")
+
+
+#Dendrogramme
+import scipy.cluster.hierarchy as shc
+# Donnees dans datanp
+print ( " Dendrogramme ’ single ’ donnees initiales " )
+linked_mat = shc.linkage ( datanp ,'single')
+plt.figure ( figsize = ( 12 , 12 ) )
+shc.dendrogram ( linked_mat ,
+orientation = 'top' ,
+distance_sort = 'descending' ,
+show_leaf_counts = False )
+plt.show ()
+
+#clustering hierarchique
+# set distance_threshold ( 0 ensures we compute the full tree )
+tps1 = time.time ()
+model = cluster.AgglomerativeClustering ( distance_threshold = 5 , linkage = 'ward' , n_clusters = None )
+model = model.fit ( datanp )
+tps2 = time.time ()
+labels = model.labels_
+k = model.n_clusters_
+leaves = model.n_leaves_
+# Affichage clustering avec distance 
+plt.scatter ( f0 , f1 , c = labels , s = 8 )
+plt.title ( " Resultat du clustering " )
+plt.show ()
+print ( " nb clusters = " ,k , " , nb feuilles = " , leaves , " runtime = " , round (( tps2 - tps1 ) * 1000 , 2 ) ," ms " )
+# set the number of clusters
+k = 4
+tps1 = time.time ()
+model = cluster.AgglomerativeClustering ( linkage = 'single' , n_clusters = k )
+model = model.fit ( datanp )
+tps2 = time.time ()
+labels = model.labels_
+kres = model.n_clusters_
+leaves = model.n_leaves_
+
+# Affichage clustering avec k
+plt.scatter ( f0 , f1 , c = labels , s = 8 )
+plt.title ( " Resultat du clustering " )
+plt.show ()
+print ( " nb clusters = " ,k , " , nb feuilles = " , leaves , " runtime = " , round (( tps2 - tps1 ) * 1000 , 2 ) ," ms " )
